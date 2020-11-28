9c5b38e8482f7b04ca32460dcd842d720b97ff9d
Joseph <jdolph211@gmail.com>
2020-11-27 20:40:56 -0600

    added search bar

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import React from  'react';
 import CardList from './CardList';
 import { robots } from './robots';
+import Searchbox from './searchbox';
 
 const App = () => {
     return (
         <div className = 'tc'>
-        <h2>Robot Friends</h2>
-         <CardList robots = { robots } />
+            <h2>Robot Friends</h2>
+            <Searchbox />
+            <CardList robots = { robots } />
         </div>
     );
 }
