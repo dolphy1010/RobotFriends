fa275c9444d8134ad6ea078628601d835058893c
Joseph <jdolph211@gmail.com>
2020-11-28 05:42:22 -0600

    searchbox memory added

--- src/app.js
+++ src/app.js
@@ -1,16 +1,34 @@
-import React from  'react';
+import React from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import Searchbox from './searchbox';
 
-const App = () => {
-    return (
-        <div className = 'tc'>
-            <h2>Robot Friends</h2>
-            <Searchbox />
-            <CardList robots = { robots } />
-        </div>
-    );
+class App extends React.Component {    
+
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {     
+        this.setState({searchfield: event.target.value})
+    }
+
+    render () {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()); 
+        })
+        return (
+            <div className = 'tc'>
+                <h2>Robot Friends</h2>
+                <Searchbox searchChange={this.onSearchChange}/>
+                <CardList robots = { filteredRobots } />
+            </div>
+        );
+    }
 }
 
 export default App;
